6677f93afa38101086494b0c306549d0844ecdf7
apex-predators-axp <xapexxpredatorsx@gmail.com>
2025-05-22 18:26:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+o/*
 
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
                                                  
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Developed by axp_chief"];
+const statusMessages = ["Developed by pedro.dantas."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
